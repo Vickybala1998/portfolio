5c53bd15b54f9b834dcd6810d2046448d0e6f52c
Vickybala1998 <vickybalaautomobile@gmail.com>
2025-05-27 21:56:31 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional: Smooth scroll polyfill
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
